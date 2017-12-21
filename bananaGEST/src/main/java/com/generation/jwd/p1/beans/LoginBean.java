4ab0fabc07d1c4231aaa27d96df82fcfa8863ca1
Arranque 1 <Arranque 1@a103-pc05>
2017-12-21 11:30:54 +0100

    Added Beans

---
+++ bananaGEST/src/main/java/com/generation/jwd/p1/beans/LoginBean.java
@@ -0,0 +1,45 @@
+package com.generation.jwd.p1.beans;
+
+import java.io.Serializable;
+
+public class LoginBean  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String user;
+	private String password;
+	
+	public LoginBean() {	
+	}
+	
+//	UserBean user1 = new UserBean(
+//			"user1",
+//			"12345"
+//			);
+//	
+//	UserBean user2 = new UserBean(
+//			"user2",
+//			"32155"
+//			);
+//	
+//	UserBean[] users = {user1, user2};
+//	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean validator() {
+		return this.password.equals("admin") && this.user.equals("admin");
+	}	
+}
