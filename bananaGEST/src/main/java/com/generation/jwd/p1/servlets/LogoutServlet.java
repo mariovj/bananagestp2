8dce19344e6d33f67632745b72a10fb729c104b9
danielooloza <lozanodaniel7@gmail.com>
2017-12-21 12:09:51 +0100

    changed servlet

--- bananaGEST/src/main/java/com/generation/jwd/p1/servlets/LogoutServlet.java
+++ bananaGEST/src/main/java/com/generation/jwd/p1/servlets/LogoutServlet.java
@@ -2,12 +2,13 @@ package com.generation.jwd.p1.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+@WebServlet("/homeuser")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
