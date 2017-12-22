0470208f13631f7ed3787ec66933d5dc8e0de58e
NOxtradamuZ <borjahz92@gmail.com>
2017-12-22 13:02:29 +0100

    i create a monster

--- bananaGEST/src/main/java/com/generation/jwd/p1/servlets/LogoutServlet.java
+++ bananaGEST/src/main/java/com/generation/jwd/p1/servlets/LogoutServlet.java
@@ -6,9 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LogoutServlet
- */
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
