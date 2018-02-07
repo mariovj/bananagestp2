3e7d3932a8d04ff1eb5e8562bac36dec961006f0
Mario Neila <Mario Neila>
2018-02-07 16:01:37 +0100

    changing task on .beans package

--- bananaGEST/src/main/java/com/generation/jwd/beans/Tasks.java
+++ bananaGEST/src/main/java/com/generation/jwd/beans/Tasks.java
@@ -1,5 +1,7 @@
 package com.generation.jwd.beans;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,38 +12,83 @@ import javax.persistence.Table;
 public class Tasks {
 	
 	@Id
-	@Column(name = "id")
-	private int id;
+	@Column(name = "idtasks")
+	private int idtasks;
 	
-	@Column(name = "email")
-	private String email;
+	@Column(name = "title")
+	private String title;
 	
-	@Column(name = "password")
-	private String password;
+	@Column(name = "startdate")
+	private Date startdate;
 
-	public int getId() {
-		return id;
+	@Column(name = "finishdate")
+	private Date finishdate;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "notes")
+	private String notes;
+
+	public int getIdtasks() {
+		return idtasks;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setIdtasks(int idtasks) {
+		this.idtasks = idtasks;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
-	public String getPassword() {
-		return password;
+	public Date getStartdate() {
+		return startdate;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
 	}
+
+	public Date getFinishdate() {
+		return finishdate;
+	}
+
+	public void setFinishdate(Date finishdate) {
+		this.finishdate = finishdate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
 	
 
 }
