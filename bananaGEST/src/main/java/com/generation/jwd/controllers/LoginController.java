5a6df0520193faa9f9e638b0808ef9c5b8c540f9
mdguay123 <danielm_95@hotmail.com>
2018-02-07 12:26:54 +0100

    configure LoginController

---
+++ bananaGEST/src/main/java/com/generation/jwd/controllers/LoginController.java
@@ -0,0 +1,45 @@
+package com.generation.jwd.controllers;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.generation.jwd.beans.User;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String renderUser() {
+		
+		System.out.println("Inicio");
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		
+		User user = new User();
+		user.setName("NameHibernate");
+		user.setSurname("SurnameHibernate");
+		user.setEmail("EmailHibernate");
+		user.setPassword("PasswordHibernate");
+		user.setUserImage("UserImageHibernate");
+		
+		session.persist(user);
+		
+		t.commit();
+		
+		session.close();
+		
+		System.out.println("Fin");
+		
+		return "hello";
+	}
+}
--- bananaGEST/src/main/java/com/generation/jwd/controllers/PruebaControllers.java
+++
@@ -1,6 +0,0 @@
-package com.generation.jwd.controllers;
-
-public class PruebaControllers {
-
-	private String Controllers;
-}
