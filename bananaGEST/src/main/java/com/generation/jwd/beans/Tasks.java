e4be985f71bf62d62cc663c40af53a25f8b1721e
Mario Neila <Mario Neila>
2018-02-07 15:39:00 +0100

    Creating Task on beans package

---
+++ bananaGEST/src/main/java/com/generation/jwd/beans/Tasks.java
@@ -0,0 +1,47 @@
+package com.generation.jwd.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tasks")
+public class Tasks {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
